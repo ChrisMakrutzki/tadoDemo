47e8a2f2a44aa757afd0206d9ca251330041d6b0
Christian Makrutzki <christian@makrutzki.de>
2022-04-24 09:38:26 +0200

    Fixed formatting

--- src/app/room-overview/room-overview-routing.module.ts
+++ src/app/room-overview/room-overview-routing.module.ts
@@ -6,6 +6,6 @@ const routes: Routes = [{ path: '', component: RoomOverviewComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class RoomOverviewRoutingModule { }
+export class RoomOverviewRoutingModule {}
